cf40977c06a924196a661c1ac5a8bf7c71ae3d5d
Matt Hayden <hayden+github@morp.holo.gy>
2015-09-02 21:53:54 -0600

    OO stuff, NULL first introduced

--- Taxon.py
+++ Taxon.py
@@ -3,7 +3,7 @@
 try:
 	assert isinstance(Taxonomy, dict)
 except NameError:
-	Taxonomy = {}
+	Taxonomy = {'NULL': {'id': 0, 'name': None }}
 #
 class TaxonBaseObject:
 	def __init__(self, name, namespace=Taxonomy, **params):
@@ -21,27 +21,45 @@ class TaxonBaseObject:
 	def __repr__(self):
 		#return "<{}({})>".format(self.name, len(self.__dict__))
 		return "<{}>".format(self.name)
+	def __int__(self):
+		return self.id
+	def __bool__(self):
+		return 0 != int(self) 
 #
 try:
 	assert(isinstance(lookup_table, dict))
 except NameError:
 	lookup_table = {}
 #
+# customize:
 def name_cleaner(text):
 	text = text.strip('+_')
 	if ' ' in text:
 		text = text.replace(' ', '_')
 	return text
+#
 def tag(item, table=lookup_table, synonyms=[]):
+	N = len(Taxonomy)
 	if isinstance(item, TaxonBaseObject):
-		n = item.name
-		t = table.get(n, item)
-	else:
+		t = item
+		#n = item.name
+		#t = table.get(n, item)
+	elif isinstance(item, int):
+		id = item % N
+		for el, kv in Taxonomy.items():
+			if id == kv['id']:
+				t = tag(el)
+				break
+	elif isinstance(item, str):
 		s = name_cleaner(item)
 		if s in table:
 			t = table[s]
-		else: # won't get called until later, when TaxonObject has been declared
+		else: # won't get called until later, when TaxonObject (below) has been declared
 			t = TaxonObject(s)
+	elif item: # hope it's hashable
+		t = TaxonObject(item)
+	else:
+		t = tag(0)
 	if isinstance(synonyms, str):
 		synonyms = synonyms.split()
 	for linkname in synonyms:
@@ -51,10 +69,10 @@ def tag(item, table=lookup_table, synonyms=[]):
 class TaxonObject(TaxonBaseObject):
 	def __eq__(self, other):
 		if isinstance(other, TaxonBaseObject):
-			return self.id == other.id
+			return int(self) == int(other)
 		else:
 			assert isinstance(self, TaxonBaseObject)
 			return self.name == name_cleaner(other)
 	# if defining __eq__ then also define __hash__
 	def __hash__(self):
-		return self.id
+		return int(self)
