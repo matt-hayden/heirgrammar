6548e9963fac4e681f9c4496ef27bcaa839310a0
Matt Hayden <hayden+github@morp.holo.gy>
2015-09-18 16:16:44 -0600

    now rules/, .rules/, ../rules/ and ../.rules/ are OK (in that order)

--- cli.py
+++ cli.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 import glob
+import os, os.path
 import sys
 
 from . import *
 
+for searchme in [ 'rules', '.rules', '../rules', '../.rules' ]:
+	if os.path.isdir(searchme):
+		info("Using .rules files found in '{}'".format(searchme))
+		RULES_FILES = sorted(glob.glob(os.path.join(searchme, '*.rules')))
+		break
+else:
+	panic("No rules file found")
+	sys.exit(-1)
+parser.setup(RULES_FILES)
+
 def arrange_dirs(*args, **kwargs):
-	parser.setup(glob.glob('rules/*.rules'))
 	ha = list(shtools.hier_arrange(*args, **kwargs)) # heir_arrange is a generator of syntax lines
 	if ha:
 		print("#!/bin/bash")
