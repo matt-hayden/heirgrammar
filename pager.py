c7cd91e24a4087c5d62eb10aee13b7692a3b402c
Matt Hayden <hayden+github@morp.holo.gy>
2015-09-22 16:20:53 -0600

    same arg as redirect_stdout

--- pager.py
+++ pager.py
@@ -7,16 +7,23 @@ import subprocess
 
 PAGER = os.environ.get('PAGER', 'less')
 
+def get_pager_process(command=[PAGER]):
+	p = subprocess.Popen(command, stdin=subprocess.PIPE, universal_newlines=True)
+	return p.communicate
+
 @contextlib.contextmanager
-def pager(command_line=[PAGER], callback=None):
-	s = io.StringIO()
+def pager(s=io.StringIO(),
+		  call=(get_pager_process() if os.isatty(1) else print),
+		  callback=None):
+	
 	with contextlib.redirect_stdout(s):
-		yield command_line
-	p = subprocess.Popen(command_line, stdin=subprocess.PIPE, universal_newlines=True)
-	s.seek(0)
-	p.communicate( callback(s.read()) if callback else s.read() )
+		yield
+	bsize=s.tell()
+	if bsize:
+		s.seek(0)
+		call( callback(s.read()) if callback else s.read() )
 #
 if __name__ == '__main__':
 	with pager() as p:
 		print("Hello World")
-		print("Brought to you by {}".format(' '.join(p)))
+		print("Brought to you by the letter p")
