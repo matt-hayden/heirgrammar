05fadc0f7d11617390eaea86ffd86ca0d361ce37
Matt Hayden <hayden+github@morp.holo.gy>
2015-09-26 10:04:42 -0600

    Oops, silly assumption

--- pager.py
+++ pager.py
@@ -7,13 +7,9 @@ import subprocess
 
 PAGER = os.environ.get('PAGER', 'less')
 
-def get_pager_process(command=[PAGER]):
-	p = subprocess.Popen(command, stdin=subprocess.PIPE, universal_newlines=True)
-	return p.communicate
-
 @contextlib.contextmanager
 def pager(s=io.StringIO(),
-		  call=(get_pager_process() if os.isatty(1) else print),
+		  command=[PAGER],
 		  callback=None):
 	
 	with contextlib.redirect_stdout(s):
@@ -21,7 +17,11 @@ def pager(s=io.StringIO(),
 	bsize=s.tell()
 	if bsize:
 		s.seek(0)
-		call( callback(s.read()) if callback else s.read() )
+		if os.isatty(1):
+			p = subprocess.Popen(command, stdin=subprocess.PIPE, universal_newlines=True)
+			p.communicate( callback(s.read()) if callback else s.read() )
+		else:
+			print( callback(s.read()) if callback else s.read() )
 #
 if __name__ == '__main__':
 	with pager() as p:
