0715ceb8112d83c0a95f9f05b56a8b75b8ac0404
Matt Hayden <hayden+github@morp.holo.gy>
2015-10-02 10:36:00 -0600

    Refactored

--- pager.py
+++ pager.py
@@ -11,17 +11,19 @@ PAGER = os.environ.get('PAGER', 'less')
 def pager(s=io.StringIO(),
 		  command=[PAGER],
 		  callback=None):
-	
+	if os.isatty(1):
+		def _output(t):
+			p = subprocess.Popen(command, stdin=subprocess.PIPE, universal_newlines=True)
+			p.communicate( callback(t) if callback else t )
+	else:
+		def _output(t):
+			print( callback(t) if callback else t )
 	with contextlib.redirect_stdout(s):
 		yield
 	bsize=s.tell()
 	if bsize:
 		s.seek(0)
-		if os.isatty(1):
-			p = subprocess.Popen(command, stdin=subprocess.PIPE, universal_newlines=True)
-			p.communicate( callback(s.read()) if callback else s.read() )
-		else:
-			print( callback(s.read()) if callback else s.read() )
+		_output(s.read())
 #
 if __name__ == '__main__':
 	with pager() as p:
